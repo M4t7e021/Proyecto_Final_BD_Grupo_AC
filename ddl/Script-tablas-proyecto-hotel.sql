0c4d7c47b07ce20191b7eed694326e1fd789f243
M4t7e021 <00030123@uca.edu.sv>
2026-05-12 22:03:29 -0600

    feat: Subiendo script sobre la creacion de las bases de datos y subiendo las carpetas para tener orden en el proyecto

---
+++ ddl/Script-tablas-proyecto-hotel.sql
@@ -0,0 +1,132 @@
+create table hotel(
+id_hotel serial primary key,
+nombre varchar(100) not null,
+direccion varchar(200) not null,
+telefono varchar(20) unique not null 
+);
+
+create table tipo_habitacion(
+id_tipo serial primary key,
+nombre_tipo varchar(100) check (nombre_tipo in ('Simple' , 'Doble' , 'Suite')),
+precio_noche decimal(10,2) check (precio_noche > 0 ),
+capacidad int check (capacidad > 0 )
+);
+
+create table habitacion(
+id_habitacion serial primary key,
+numero varchar(10) unique not null,
+piso int check (piso > 0 ),
+estado varchar(20) default 'Disponible'
+check (estado in ('Disponible' , 'Ocupada' , 'Mantenimiento')),
+id_hotel int not null,
+id_tipo int not null,
+
+foreign key (id_hotel)
+references hotel(id_hotel),
+foreign key (id_tipo)
+references tipo_habitacion(id_tipo)
+);
+
+create table huesped(
+id_huesped serial primary key,
+nombre varchar(50) not null,
+apellido varchar(50) not null,
+dui varchar(10) unique not null,
+telefono varchar(15),
+correo varchar(100) unique not null
+);
+
+create table reservacion(
+id_reservacion serial primary key,
+fecha_reserva date default current_date,
+fecha_inicio date not null,
+fecha_fin date not null,
+estado varchar(20),
+id_huesped int not null,
+id_habitacion int not null,
+
+foreign key (id_huesped)
+references huesped(id_huesped),
+foreign key (id_habitacion)
+references habitacion(id_habitacion)
+);
+
+create table empleado(
+id_empleado serial primary key,
+nombre varchar(100),
+cargo varchar(50),
+telefono varchar(15)
+);
+
+create table checkin_checkout(
+id_check serial primary key,
+fecha_checkin timestamp,
+fecha_checkout timestamp,
+id_reservacion int not null,
+id_empleado int not null,
+
+foreign key (id_reservacion)
+references reservacion(id_reservacion),
+foreign key (id_empleado)
+references empleado(id_empleado)
+);
+
+create table servicio(
+id_servicio serial primary key,
+nombre varchar(100),
+precio decimal(10,2)
+);
+
+create table consumo_servicio(
+id_consumo serial primary key,
+cantidad int,
+subtotal decimal(10,2),
+fecha_consumo date,
+id_servicio int not null,
+id_reservacion int not null,
+
+foreign key (id_servicio)
+references servicio(id_servicio),
+foreign key (id_reservacion)
+references reservacion(id_reservacion)
+);
+
+create table factura(
+id_factura serial primary key,
+fecha_factura date,
+total decimal(10,2),
+metodo_pago varchar(30),
+id_reservacion int not null,
+
+foreign key (id_reservacion)
+references reservacion(id_reservacion)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
